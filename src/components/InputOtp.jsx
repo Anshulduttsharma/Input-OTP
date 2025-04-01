d3256411eb20a3cf046772846b16176c5bce8dd5
Anshul <anshuls26@gmail.com>
2025-04-01 17:54:19 +0530

    use useRef for references of each inputs

--- src/components/InputOtp.jsx
+++ src/components/InputOtp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 const inputOtp = () => {
   const OTP_DIGITS_COUNT = 5;
@@ -6,11 +6,30 @@ const inputOtp = () => {
     new Array(OTP_DIGITS_COUNT).fill("")
   );
 
+  const refArr = useRef([]);
+
+  const handleOnChange = (value, index) => {
+    if (isNaN(value)) return;
+    console.log(value);
+    const newArr = [...inputArr];
+    newArr[index] = value.slice(-1);
+    setInputArr(newArr);
+  };
+
   return (
     <div>
       <h1>Validate OTP</h1>
       {inputArr.map((input, index) => {
-        return <input className="otp-input" key={index} type="text"  value={inputArr[index]}/>;
+        return (
+          <input
+            className="otp-input"
+            key={index}
+            type="text"
+            ref={(input) => (refArr.current[index] = input)}
+            value={inputArr[index]}
+            onChange={(e) => handleOnChange(e.target.value, index)}
+          />
+        );
       })}
     </div>
   );
